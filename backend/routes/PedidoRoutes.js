3255a9a4555483d3feff40c606b6f59f5708df7f
Valentino <valentinosanchezraverta@gmail.com>
2024-05-27 15:50:18 +0200

    Bug solucionados

--- backend/routes/PedidoRoutes.js
+++ backend/routes/PedidoRoutes.js
@@ -27,12 +27,25 @@ router.get('/', (req, res) => {
     });
 });
 
-
-
 // Obtener todos los pedidos de un cliente
 router.get('/cliente/:id', (req, res) => {
     const { id } = req.params;
-    db.query('SELECT p.id_pedido, p.coste, p.tipo_pago, p.fecha, GROUP_CONCAT(DISTINCT h.nombre ORDER BY h.nombre ASC) AS hamburguesa, GROUP_CONCAT(DISTINCT a.tipo_alergeno ORDER BY a.tipo_alergeno ASC) AS alergenos FROM pedido p INNER JOIN pedido_esta_hamburguesa peh ON p.id_pedido = peh.pedido_id_pedido INNER JOIN hamburguesa h ON peh.hamburguesa_id_hamburguesa = h.id_hamburguesa LEFT JOIN hamburguesa_tiene_ingrediente hti ON h.id_hamburguesa = hti.hamburguesa_id_hamburguesa LEFT JOIN ingrediente i ON hti.ingrediente_id_ingrediente = i.id_ingrediente LEFT JOIN alergenos a ON i.alergenos_id_alergeno = a.id_alergeno WHERE p.cliente_id_cliente = ? GROUP BY p.id_pedido ORDER BY p.id_pedido ASC;', [id], (err, results) => {
+    const query = `
+        SELECT p.id_pedido, p.coste, p.tipo_pago, p.fecha, 
+        GROUP_CONCAT(DISTINCT h.nombre ORDER BY h.nombre ASC) AS hamburguesa, 
+        GROUP_CONCAT(DISTINCT a.tipo_alergeno ORDER BY a.tipo_alergeno ASC) AS alergenos 
+        FROM pedido p 
+        INNER JOIN pedido_esta_hamburguesa peh ON p.id_pedido = peh.pedido_id_pedido 
+        INNER JOIN hamburguesa h ON peh.hamburguesa_id_hamburguesa = h.id_hamburguesa 
+        LEFT JOIN hamburguesa_tiene_ingrediente hti ON h.id_hamburguesa = hti.hamburguesa_id_hamburguesa 
+        LEFT JOIN ingrediente i ON hti.ingrediente_id_ingrediente = i.id_ingrediente 
+        LEFT JOIN alergenos a ON i.alergenos_id_alergeno = a.id_alergeno 
+        WHERE p.cliente_id_cliente = ? 
+        GROUP BY p.id_pedido 
+        ORDER BY p.id_pedido ASC;
+    `;
+
+    db.query(query, [id], (err, results) => {
         if (err) {
             console.error('Error al obtener los pedidos:', err);
             return res.status(500).json({ error: 'Error al obtener los pedidos' });
@@ -43,8 +56,7 @@ router.get('/cliente/:id', (req, res) => {
 
 router.put('/:id', (req, res) => {
     const { id } = req.params;
-    console.log("llega")
-    db.query('UPDATE pedido SET entregado = 1 WHERE id_pedido = ?', [ id], (err, result) => {
+    db.query('UPDATE pedido SET entregado = 1 WHERE id_pedido = ?', [id], (err, result) => {
         if (err) {
             console.error('Error al actualizar el pedido:', err);
             return res.status(500).json({ error: 'Error al actualizar el pedido' });
@@ -52,7 +64,26 @@ router.put('/:id', (req, res) => {
         if (result.affectedRows === 0) {
             return res.status(404).json({ error: 'Pedido no encontrado' });
         }
-        res.json({ message: 'Pedido actualizado exitosamente' });
+
+        db.query('SELECT MAX(category_id) AS maxId FROM log', (err, result) => {
+            if (err) {
+                console.error('Error al obtener el max id_log:', err);
+                return res.status(500).json({ error: 'Error al obtener el max id_log' });
+            }
+
+            const newIdLog = result[0].maxId + 1;
+            const formattedDate = formatDate(new Date());
+            const content = `El pedido con id ${id} se ha entregado de manera exitosa`;
+            
+            const insertQuery = 'INSERT INTO log (category_id, id_log, fecha, contenido) VALUES (?, ?, ?, ?)';
+            db.query(insertQuery, [newIdLog, 1, formattedDate, content], (err) => {
+                if (err) {
+                    console.error('Error al insertar en log:', err);
+                    return res.status(500).json({ error: 'Error al insertar en log' });
+                }
+                res.json({ message: 'Pedido actualizado y log insertado exitosamente' });
+            });
+        });
     });
 });
 
@@ -60,27 +91,39 @@ router.put('/:id', (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const id_pedido = await MaxIdPedido() + 1;
-
         const id_casa = await compareDireccion(req.body.direccion, req.body.id_cliente);
         const currentDate = new Date();
         const formattedDate = currentDate.toISOString().split('T')[0];
         const sql = 'INSERT INTO pedido (id_pedido,coste, cliente_id_cliente, casa_id_casa,tipo_pago,fecha,entregado) VALUES (?, ?, ?,?,?,?,?)';
 
-        db.query(sql, [id_pedido, req.body.contenido.totalPrice, req.body.id_cliente, id_casa, req.body.tipo_pago, formattedDate,0], (err, result) => {
+        db.query(sql, [id_pedido, req.body.contenido.totalPrice, req.body.id_cliente, id_casa, req.body.tipo_pago, formattedDate, 0], (err) => {
             if (err) {
                 console.error('Error al insertar el pedido:', err);
                 return res.status(500).json({ error: 'Error al insertar el pedido' });
             }
-            for (let index = 0; index < req.body.contenido.items.length; index++) {
-                let sql2 = 'INSERT INTO pedido_esta_hamburguesa (pedido_id_pedido, hamburguesa_id_hamburguesa) VALUES (?,?)';
-                db.query(sql2, [id_pedido, req.body.contenido.items[index].food.id_hamburguesa], (err, result) => {
-                    if (err) {
-                        console.error('Error al insertar el pedido:', err);
-                        return res.status(500).json({ error: 'Error al insertar el pedido' });
-                    }
+
+            const promises = req.body.contenido.items.map(item => {
+                const sql2 = 'INSERT INTO pedido_esta_hamburguesa (pedido_id_pedido, hamburguesa_id_hamburguesa) VALUES (?,?)';
+                return new Promise((resolve, reject) => {
+                    db.query(sql2, [id_pedido, item.food.id_hamburguesa], (err) => {
+                        if (err) {
+                            console.error('Error al insertar el pedido:', err);
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
                 });
-            }
-            res.status(201).json({ message: 'Pedido insertado correctamente' });
+            });
+
+            Promise.all(promises)
+                .then(() => {
+                    res.status(201).json({ message: 'Pedido insertado correctamente' });
+                })
+                .catch(error => {
+                    console.error('Error al insertar los elementos del pedido:', error);
+                    res.status(500).json({ error: 'Error al insertar los elementos del pedido' });
+                });
         });
 
     } catch (error) {
@@ -88,9 +131,6 @@ router.post('/', async (req, res) => {
         res.status(500).json({ error: 'Error al procesar el pedido' });
     }
 });
-
-
-
 
 function MaxIdPedido() {
     return new Promise((resolve, reject) => {
@@ -100,19 +140,14 @@ function MaxIdPedido() {
                 reject(err);
             } else {
                 if (result.length > 0) {
-                    const id = result[0].id;
-                    resolve(id);
+                    resolve(result[0].id);
                 } else {
-                    console.log("No se encontraron resultados.");
-                    resolve(null);
+                    resolve(0);
                 }
             }
         });
     });
 }
-
-
-
 
 function MaxIdCasa() {
     return new Promise((resolve, reject) => {
@@ -122,11 +157,9 @@ function MaxIdCasa() {
                 reject(err);
             } else {
                 if (result.length > 0) {
-                    const id = result[0].id;
-                    resolve(id);
+                    resolve(result[0].id);
                 } else {
-                    console.log("No se encontraron resultados.");
-                    resolve(null);
+                    resolve(0);
                 }
             }
         });
@@ -143,13 +176,13 @@ async function compareDireccion(direccion, id_cliente) {
                 MaxIdCasa().then(id_casa => {
                     id_casa += 1;
                     let sql = 'INSERT INTO casa (id_casa, direccion) VALUES (?, ?)';
-                    db.query(sql, [id_casa, direccion], (err, result) => {
+                    db.query(sql, [id_casa, direccion], (err) => {
                         if (err) {
                             console.error('Error al insertar la casa:', err);
                             reject(err);
                         } else {
-                            let sql2 = 'INSERT INTO cliente_tiene_casa (id_cliente,id_casa) VALUES (?, ?)';
-                            db.query(sql2, [id_cliente, id_casa], (err, result) => {
+                            let sql2 = 'INSERT INTO cliente_tiene_casa (id_cliente, id_casa) VALUES (?, ?)';
+                            db.query(sql2, [id_cliente, id_casa], (err) => {
                                 if (err) {
                                     console.error('Error al insertar la casa:', err);
                                     reject(err);
@@ -167,5 +200,13 @@ async function compareDireccion(direccion, id_cliente) {
         });
     });
 }
+
+const formatDate = (date) => {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const day = String(d.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+};
 
 module.exports = router;
